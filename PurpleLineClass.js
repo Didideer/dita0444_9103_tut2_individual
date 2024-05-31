4293ebf0e18b8a1d4f037be1f00aee6242e15433
Tiandideer <Tiandideer@gmail.com>
2024-05-31 16:05:31 +1000

    MovingCircle class to represent circles that move along the purple lines
    MovingCircle class to represent circles that move along the purple lines

--- PurpleLineClass.js
+++ PurpleLineClass.js
@@ -1,3 +1,4 @@
+// Extend the PurpleLine class
 class PurpleLine {
     constructor(x, y, w, h) {
         this.x = x;
@@ -21,42 +22,10 @@ class PurpleLine {
         this.drawW = this.w * insideCanvasWidth;
         this.drawH = this.h * insideCanvasHeight;
     }
+
     // Method to check if a point (mouseX, mouseY) is inside the purple line rectangle
     isClicked(mouseX, mouseY) {
         return mouseX > this.drawX && mouseX < this.drawX + this.drawW && mouseY > this.drawY && mouseY < this.drawY + this.drawH;
     }
 }
 
-// MovingCircle class to represent circles that move along the purple lines
-class MovingCircle {
-    constructor(purpleLine, color) {
-        this.purpleLine = purpleLine; // The purple line the circle will move along
-        this.color = color; // Color of the circle
-        this.radius = Math.min(purpleLine.drawW, purpleLine.drawH) / 2; // Radius of the circle, based on the smaller dimension of the rectangle
-        this.x = purpleLine.drawX; // Initial x position of the circle
-        this.y = purpleLine.drawY; // Initial y position of the circle
-        this.speed = 2; // Speed at which the circle moves
-        this.direction = {
-            x: purpleLine.drawW > 0 ? 1 : -1, // Direction of movement along the x-axis
-            y: purpleLine.drawH > 0 ? 1 : -1  // Direction of movement along the y-axis
-        };
-    }
-}
-
-
-
-// Method to move the circle along the purple line
-move(); {
-    // Move along the x-axis
-    if (this.direction.x > 0) {
-        this.x += this.speed;
-        if (this.x > this.purpleLine.drawX + this.purpleLine.drawW) {
-            this.x = this.purpleLine.drawX;
-        }
-    } else {
-        this.x -= this.speed;
-        if (this.x < this.purpleLine.drawX) {
-            this.x = this.purpleLine.drawX + this.purpleLine.drawW;
-        };
-    }
-}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dita0444_9103_tut2_individual
 personal work
+
+# my work is about interaction by click the purpul line to have moving circles
+
+## all of my individual part are added in purpleLineClass.js
+
+### add isClicked
